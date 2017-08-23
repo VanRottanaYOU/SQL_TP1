6ce052ec826fd9fe7648c37e14b9eed4b3828826
VanRottanaYOU <vr.you@hotmail.fr>
2017-08-23 15:26:24 +0200

    mise en forme

--- src/main/java/fr/codevallee/formation/tp1/Connect.java
+++ src/main/java/fr/codevallee/formation/tp1/Connect.java
@@ -28,55 +28,9 @@ public class Connect {
 	
 	public void getConnexion() throws Exception {
 		try {
-//			déjà dans les dépendances pom.xml
-//			Class.forName("com.mysql.jdbc.Driver");
 			connect = DriverManager.getConnection(url, user, passwd);
-			// Step 2: Allocate a 'Statement' object in the Connection
-	         Statement stmt = connect.createStatement();
-
-	         // Step 3: Execute a SQL SELECT query, the query result
-	         //  is returned in a 'ResultSet' object.
-	         String strSelect = "select * from agence";
-	         System.out.println("The SQL query is: " + strSelect); // Echo For debugging
-	         System.out.println();
-	 
-	         ResultSet rset = stmt.executeQuery(strSelect);
-	 
-	         // Step 4: Process the ResultSet by scrolling the cursor forward via next().
-	         //  For each row, retrieve the contents of the cells with getXxx(columnName).
-	         System.out.println("The records selected are:");
-	         int rowCount = 0;
-	         while(rset.next()) {   // Move the cursor to the next row, return false if no more row
-	            String idAgence = rset.getString("idAgence");
-	            String nom = rset.getString("nom");
-	            String adresse = rset.getString("adresse");
-	            System.out.println(idAgence + ", " + nom + ", " + adresse);
-	            ++rowCount;
-	            Agence monAgence = new Agence(idAgence,nom,adresse);
-	            listAgences.add(monAgence);	          	            
-	         }
-	         stmt.close();
-	         
-	         System.out.println("Total number of records = " + rowCount);
-	         PreparedStatement recherchePersonne = 
-	 	     connect.prepareStatement("SELECT * FROM Agence WHERE nom = ?");
-
-	 	     recherchePersonne.setString(1, "fnaim");
-	 	                  	
-	 	     stmt = connect.createStatement();
-	 	     rset = recherchePersonne.executeQuery();
-
-	 	     System.out.println("parcours des données retournées");
-
-	 	     boolean encore = rset.next();
-
-	 	     while (encore) {
-	 	                   System.out.print(rset.getString(1) + " :  "+rset.getString(2)+ " "+rset.getString(3));
-	 	                   System.out.println();
-	 	                   monAgence = new Agence(rset.getString(1),rset.getString(2),rset.getString(3));
-	 	                   encore = rset.next();	 	                 	 	                    	                   
-	 	     }
-	 	    stmt.close();
+			listerAgences();
+			chercherAgence("fnaim");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -99,4 +53,68 @@ public class Connect {
 		return monAgence;
 	}
 	
+	public void listerAgences() throws SQLException{
+		// Step 2: Allocate a 'Statement' object in the Connection
+		Statement stmt;
+		try {
+			stmt = connect.createStatement();
+
+			 // Step 3: Execute a SQL SELECT query, the query result
+			 //  is returned in a 'ResultSet' object.
+			 String strSelect = "select * from agence";
+			 System.out.println("The SQL query is: " + strSelect); // Echo For debugging
+			 System.out.println();
+ 
+			 ResultSet rset = stmt.executeQuery(strSelect);
+ 
+			 // Step 4: Process the ResultSet by scrolling the cursor forward via next().
+			 //  For each row, retrieve the contents of the cells with getXxx(columnName).
+			 System.out.println("The records selected are:");
+			 int rowCount = 0;
+			 while(rset.next()) {   // Move the cursor to the next row, return false if no more row
+			    String idAgence = rset.getString("idAgence");
+			    String nom = rset.getString("nom");
+			    String adresse = rset.getString("adresse");
+			    System.out.println(idAgence + ", " + nom + ", " + adresse);
+			    ++rowCount;
+			    Agence monAgence = new Agence(idAgence,nom,adresse);
+			    listAgences.add(monAgence);	          	            
+			 }
+			 stmt.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+         
+	}
+	
+	public void chercherAgence(String nom) throws SQLException{
+		Statement stmt;
+		 try {
+			
+			 PreparedStatement recherchePersonne = 
+			 connect.prepareStatement("SELECT * FROM Agence WHERE nom = ?");
+
+			 recherchePersonne.setString(1, nom);
+			              	
+			 stmt = connect.createStatement();
+			 ResultSet rset = recherchePersonne.executeQuery();
+
+			 System.out.println("parcours des données retournées");
+
+			 boolean encore = rset.next();
+
+			 while (encore) {
+			               System.out.print(rset.getString(1) + " :  "+rset.getString(2)+ " "+rset.getString(3));
+			               System.out.println();
+			               monAgence = new Agence(rset.getString(1),rset.getString(2),rset.getString(3));
+			               encore = rset.next();	 	                 	 	                    	                   
+			 }
+			 stmt.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 	    
+	}
 }
